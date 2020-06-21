4b18345cb78fd95ff472962d5aa2e33046122888
junda1991 <43408101+junda1991@users.noreply.github.com>
2020-06-21 15:43:48 +0800

    Update DemoApplication.java

--- src/main/java/com/zjd/demo/DemoApplication.java
+++ src/main/java/com/zjd/demo/DemoApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+         System.out.println("111");
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("111");
     }
 
 }
